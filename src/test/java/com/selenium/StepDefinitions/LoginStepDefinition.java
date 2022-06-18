fcbb206aabcdd0c1f2b4b7fe32fc1375dda87638
vrt999 <ravi4a4@gmail.com>
2022-06-18 15:59:00 +0530

    master new commit added

--- src/test/java/com/selenium/StepDefinitions/LoginStepDefinition.java
+++ src/test/java/com/selenium/StepDefinitions/LoginStepDefinition.java
@@ -88,4 +88,11 @@ public class LoginStepDefinition {
 
 	}
 
+	@Then("User should be loggedin successfully new master updated")
+	public void user_should_be_loggedin_develop_master_new() {
+		driver.findElement(By.cssSelector("button[type = 'submit']")).click();
+		System.out.println("Step 3 is executed successfully commit from develop and master");
+
+	}
+
 }
